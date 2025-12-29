954cbcc38109d6e552d4cf56df41f44350e8d9a9
expertguy <99809774+expertguy@users.noreply.github.com>
2025-12-29 14:40:53 +0500

    Updated WhyStngr.js

--- src/components/WhyStngr.js
+++ src/components/WhyStngr.js
@@ -9,10 +9,10 @@ export function WhyStngr() {
         <div class="blur-section-cards">
           <div class="blur-card">
             <div class="blur-card-icon">
-              <svg width="80" height="80" viewBox="0 0 80 80" fill="none">
-                <rect x="10" y="6.67" width="60" height="33.33" stroke="white" stroke-width="1.5"/>
-                <rect x="10" y="40" width="60" height="16.67" stroke="white" stroke-width="1.5"/>
-                <rect x="10" y="56.67" width="60" height="16.67" stroke="white" stroke-width="1.5"/>
+              <svg width="80" height="80" viewBox="0 0 80 80" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M40 6.66675L10 23.3334L40 40.0001L70 23.3334L40 6.66675Z" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                <path d="M10 40L40 56.6667L70 40" stroke="white" stroke-width="1.5" stroke-linejoin="round"/>
+                <path d="M10 56.6667L40 73.3334L70 56.6667" stroke="white" stroke-width="1.5" stroke-linejoin="round"/>
               </svg>
             </div>
             <div class="blur-card-content">
@@ -22,8 +22,8 @@ export function WhyStngr() {
           </div>
           <div class="blur-card">
             <div class="blur-card-icon">
-              <svg width="80" height="80" viewBox="0 0 80 80" fill="none">
-                <rect x="10" y="6.67" width="60" height="66.67" stroke="white" stroke-width="1.5"/>
+              <svg width="80" height="80" viewBox="0 0 80 80" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M70 16.6667C62.9287 10.5668 52.117 6.66675 40 6.66675C27.8831 6.66675 17.0712 10.5668 10 16.6667V36.6667C10 63.3334 40 73.3334 40 73.3334C40 73.3334 70 63.3334 70 36.6667V16.6667Z" stroke="white" stroke-width="1.5" stroke-linecap="square"/>
               </svg>
             </div>
             <div class="blur-card-content">
@@ -33,8 +33,8 @@ export function WhyStngr() {
           </div>
           <div class="blur-card">
             <div class="blur-card-icon">
-              <svg width="80" height="80" viewBox="0 0 80 80" fill="none">
-                <rect x="16.68" y="8.66" width="46.60" height="60" stroke="white" stroke-width="1.5"/>
+              <svg width="80" height="80" viewBox="0 0 80 80" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M59.9997 8.65894L46.6947 31.9424C46.682 31.9646 46.6984 31.9923 46.7237 31.9923H63.2527C63.2824 31.9923 63.2971 32.0282 63.2761 32.0492L26.6664 68.6591L33.3227 42.0337C33.3279 42.0127 33.312 41.9924 33.2903 41.9924H16.7179C16.6937 41.9924 16.6776 41.9674 16.6875 41.9454L31.6664 8.65894H59.9997Z" stroke="white" stroke-width="1.5"/>
               </svg>
             </div>
             <div class="blur-card-content">
