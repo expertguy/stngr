e1f77c4cc263e988fb3a91eb669e105ed81d9bb5
expertguy <99809774+expertguy@users.noreply.github.com>
2025-12-29 20:24:01 +0500

    Updated newsletter.css

--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -1,11 +1,10 @@
+import logo from "../assets/logo.png"
+
 export function Newsletter() {
   return `
     <section class="newsletter" id="newsletter">
       <div class="newsletter-icon">
-        <svg width="80" height="80" viewBox="0 0 80 80" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M6.66675 11.6665H73.3334V68.3332H6.66675V11.6665Z" stroke="white" stroke-width="2" stroke-linejoin="round"/>
-          <path d="M6.66675 23.3335L40.0001 40.0002L73.3334 23.3335" stroke="white" stroke-width="2"/>
-        </svg>
+        <img src="${logo}" alt="STNGR logo" class="newsletter-logo" />
       </div>
       <div class="newsletter-content">
         <h2 class="newsletter-title">Be First to Know</h2>
